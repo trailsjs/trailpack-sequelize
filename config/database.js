07e0ef8ae2c5713ba85e71a41ddd86f30de0db7c
jaumard <ioquoo3H>
2016-06-29 16:35:48 +0200

    v1.0.3

---
+++ config/database.js
@@ -0,0 +1,11 @@
+/**
+ * Database Configuration
+ * (app.config.database)
+ *
+ * Configure the ORM layer, connections, etc.
+ *
+ * @see {@link http://trailsjs.io/doc/config/database}
+ */
+module.exports = {
+  //Leave the file even empty because if the file is not present, env config is not merged
+}
